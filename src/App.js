1f002ca34cd7c2aece16154a2a6b884b7a4178ef
Your Name <you@example.com>
2024-12-27 22:59:43 +0530

    Updated dependencies and resolved vulnerabilities

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Corrected import
+
 import CalendarView from "./components/CalendarView";
 import AdminDashboard from "./components/Admin/AdminDashboard"; // Import AdminDashboard
 
